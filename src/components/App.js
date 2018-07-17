f52445fdb20a6476cc2701b6bfa8c56f7e8c7390
Chris Adams <cadams@callyo.com>
2018-07-17 10:53:45 -0400

    React app now rendering

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+"use strict"
+import React, { Component } from "react"
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <p>
+                    Hello React Router!
+                </p>
+            </div>
+        )
+    }
+}
+
+export default App
